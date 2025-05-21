ec4657888387f888d3f2f19134e6eb7fbd4b31d8
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2025-05-21 10:29:37 +0000

    added email HTML

--- script.js
+++ script.js
@@ -10,15 +10,18 @@ function validateForm() {
   
 
     // Task 3: Validate Username - Minimum 3 characters
-   if (username.length < 3) {
+   if (username.length == 0) {
+    alert("User name is required");
+   } else if (username.length < 3) {
     alert("User name must be at least three characters long.");
    }
 
-
-
     // Task 4: Validate Email - Must be a valid email format
-
-
+   // pause to discuss regular expressions
+   // go to regexr.com
+   // hand out cheat sheet
+   // build regex to check for email address
+   // what do we know about email address format?
 
 
     // Task 5: Validate Phone Number - 10 digits in 123-456-7890 format
